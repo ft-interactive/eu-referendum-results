f986e2c937a5b590aa2936c140ccf938aaa4073b
Martin Stabe <martinstabe@gmail.com>
2016-04-22 18:46:03 +0100

    tidy up commenting

--- scripts/getData.sh
+++ scripts/getData.sh
@@ -5,7 +5,6 @@ rm -rf data/pa/nominations
 (echo '"pa_id","name"') >data/ft/paCodes.csv;
 (curl -s 'http://election.pressassociation.com/Advisories/EU_voting_areas_by-region_2016.php' | pup '#main pre text{}' | sed '/^\s*$/d;s/^ *//g;;s/\.\ /,/g' | sed s/"\&amp\;"/"and"/g | csvsort -c 2) >>data/ft/paCodes.csv
 
-
 #get ONS GSS codes for 381 voting areas
 curl -s https://mapit.mysociety.org/areas/LBO,DIS,UTA,MTD,COI | jq '[.[] | {my_society_id:.id,ons_id:.codes.gss,name:.name}]' | in2csv -f json | sed -E s/"( ){0,1}(Borough|District|City){0,1} (Council|Corporation)$"//g >data/ft/gssCodes.csv
 
@@ -16,14 +15,13 @@ cat data/ft/gssCodes.csv | sed s/" County"//g | sed s/"City of Edinburgh"/"Edinb
 cat data/ft/paCodes.csv | sed s/"Anglesey"/"Isle of Anglesey"/1 | sed s/"Comhairle Nan Eilean Siar"/"Na h-Eileanan Siar"/1 | sed s/"Kingston-upon-Hull"/"Hull"/1 | sed s/"Kings Lynn and West Norfolk"/"King\'s Lynn and West Norfolk"/1 | sed s/"Windsor and Maidenhead Royal"/"Windsor and Maidenhead"/1 >data/ft/paCodes2.csv
 
 #Add Gibraltar and Northern Ireland to GSS dataset
-
 echo "9999,G99999999,Gibraltar" >>data/ft/gssCodes2.csv
 echo "8888,N88888888,Northern Ireland" >>data/ft/gssCodes2.csv
 
-#join datasets to make master lookup
+#join datasets and clean up column order to make master lookup
 csvjoin -c 2,3 data/ft/paCodes2.csv data/ft/gssCodes2.csv | csvcut -c 1,4,3,2 | csvsort -c 4 >data/ft/lookup.csv
 
-#clean up
+#clean up temporary data files
 rm data/ft/gssCodes.csv
 rm data/ft/gssCodes2.csv
 rm data/ft/paCodes.csv
