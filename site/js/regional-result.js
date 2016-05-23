848f9581c15d74ab579c9de2c85b2c84fe7895b0
Laura Carvajal <laura.carvajal@gmail.com>
2016-05-23 11:03:55 +0100

    Initial setup

--- site/js/regional-result.js
+++ site/js/regional-result.js
@@ -3,14 +3,20 @@ console.log('regional results');
 d3.json('dummyresult/regional.json', drawRegionalResultTable);
 
 function drawRegionalResultTable(results){
-    var dataJoin = d3.select('.regional-result')
+    var dataJoin = 
+
+    d3.select('.regional-result')
         .append('table')
-            .selectAll('tr')
-        .data(results)
+        .selectAll('tr')
+        .data(results.sort(function (first, second) {
+        	return (second.leave_pct - second.remain_pct) - (first.leave_pct - first.remain_pct)
+  		}))
         .call(function(join){
-            var rows = join.enter()
-                .append('tr')
-            // etc...
+        	console.log('join', join.enter())
+
+            var rows = join.enter().append('tr').text(function (d) {
+            	return d.leave_pct - d.remain_pct
+            })
         });
         
 }
