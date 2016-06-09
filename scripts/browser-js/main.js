78f067bf9fe4d7008c6306cdecb65852681462b9
tomgp <tom.g.pearson@gmail.com>
2016-06-09 12:33:45 +0100

    improve pointer appearance and handling in the map

--- scripts/browser-js/main.js
+++ scripts/browser-js/main.js
@@ -91,13 +91,11 @@ mapframe.selectAll('.area').data(localResults)
     });
 
 selectionDispatcher.on('select.map', function(d){
-    console.log('selected', d);
     var bounds = d3.select('#area-' + d.local_id).node().getBBox();
-    console.log(bounds);
     var highlightData = [{
             cx:bounds.x + bounds.width/2,
             cy:bounds.y + bounds.height/2,
-            r:Math.max(bounds.height/2, bounds.width/2),
+            r:Math.max(10, Math.max(bounds.height/2, bounds.width/2)),
         }];
     
     var join = mapframe.selectAll('g.highlight-area')
@@ -111,14 +109,14 @@ selectionDispatcher.on('select.map', function(d){
         });
     
     join.transition().select('circle.highlight-background')
-        .attr('r',function(d){ return d.r; })
-        .attr('cx',function(d){ return d.cx; })
-        .attr('cy',function(d){ return d.cy; });
+        .attr('r', function(d){ return d.r; })
+        .attr('cx', function(d){ return d.cx; })
+        .attr('cy', function(d){ return d.cy; });
     
     join.transition().select('circle.highlight-foreground')
-        .attr('r',function(d){ return d.r; })
-        .attr('cx',function(d){ return d.cx; })
-        .attr('cy',function(d){ return d.cy; });
+        .attr('r', function(d){ return d.r; })
+        .attr('cx', function(d){ return d.cx; })
+        .attr('cy', function(d){ return d.cy; });
 
     selectArea(d.local_id, d.region_id);
 });
