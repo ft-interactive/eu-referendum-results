5750e631f141f75d3b356dd7af5f011e580ca52e
Kavanagh, Luke <Luke.Kavanagh@ft.com>
2016-06-21 15:32:03 +0100

    Remove need for babel on script

--- bin/results.js
+++ bin/results.js
@@ -1,13 +1,19 @@
-// import yargs from 'yargs';
-import results from '../lib/result';
-import path from 'path';
+const results = require('../build/result').default;
+const next = require('../build/next-front-page');
+const path = require('path');
 
 const results_dir = path.resolve(process.cwd(), process.argv[2]);
 const override_dir = path.resolve(process.cwd(), process.argv[3]);
 const output_dir = path.resolve(process.cwd(), process.argv[4]);
 
-results(results_dir, override_dir, output_dir).then(() => {
+results(results_dir, override_dir, output_dir)
+.then(() => {
+  return Promise.resolve(true);
+  // return next(output_dir, data)
+})
+.then(() => {
   console.log('Done');
-}).catch(reason => {
+})
+.catch(reason => {
   console.error(reason);
 })
