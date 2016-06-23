703b4a1da011a169a3225feda0c19d94091e9fe9
Kavanagh, Luke <Luke.Kavanagh@ft.com>
2016-06-23 03:53:10 +0100

    Next home page JSON builder

--- bin/results.js
+++ bin/results.js
@@ -1,15 +1,18 @@
 const results = require('../build/result').default;
-const next = require('../build/next-front-page');
+const next = require('../build/next-front-page').default;
 const path = require('path');
 
 const results_dir = path.resolve(process.cwd(), process.argv[2]);
 const override_dir = path.resolve(process.cwd(), process.argv[3]);
 const output_dir = path.resolve(process.cwd(), process.argv[4]);
 
+console.log('results', results_dir);
+console.log('overrides', override_dir);
+console.log('output', output_dir);
+
 results(results_dir, override_dir, output_dir)
-.then(() => {
-  return Promise.resolve(true);
-  // return next(output_dir, data)
+.then((data) => {
+  return next(output_dir, data)
 })
 .then(() => {
   console.log('Done');
